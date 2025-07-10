f72b60b559412fc5ba16531d3721bc0b0f9414fb
jacklaing123@hotmail.co.uk <jacklaing123@hotmail.co.uk>
2025-07-10 10:26:15 +0100

    Added mini_project.js

---
+++ Wk2d4/mini_project/mini_project.js
@@ -0,0 +1,16 @@
+const prompt = require("readline-sync");
+
+/*
+This Project is to demonstrate a basic CLI based banking system within JS.
+
+Classes:
+    * Customer -    This stores personal data to create the 'profile' of the individual, as well as their Accounts.
+    * Account -     This stores banking info like balance, transactions etc.
+    * Transaction - This stores transaction info like sender and recipient Ids and transaction amount
+
+
+
+*/
+
+
+class customer
