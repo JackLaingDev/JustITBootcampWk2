aee0c9df2ce40c6467480ffd4ab920c01d278b3d
jacklaing123@hotmail.co.uk <jacklaing123@hotmail.co.uk>
2025-07-10 10:54:00 +0100

    Added boilerplate for services

--- Wk2d4/mini_project/mini_project.js
+++ Wk2d4/mini_project/mini_project.js
@@ -3,7 +3,15 @@ const prompt = require("readline-sync");
 /*
 This Project is to demonstrate a basic CLI based banking system within JS.
 
-Classes:
+Overall System:
+    * Bank -       This acts as a controller for the overall flow of the program using the services
+
+Services:
+    * custService - This handles all customer business logic (create customer, remove customer, etc.)
+    * accService - This handles all account business logic (withdraw, deposit, etc.)
+    * transService - This handles all transaction business logic (make transfer, etc.)
+
+Models:
     * Customer -    This stores personal data to create the 'profile' of the individual, as well as their Accounts.
     * Account -     This stores banking info like balance, transactions etc.
     * Transaction - This stores transaction info like sender and recipient Ids and transaction amount
@@ -12,8 +20,21 @@ Classes:
 
 */
 
+let accounts = []                                                // Needed as a temp database, accounts.length will allocate the id
 let customers = [];                                              // Needed as a temp database, customers.length will allocate the id
+let transactions = [];                                           // Needed as a temp database, transactions.length will allocate the id
 
+
+// Overall system
+class Bank{
+
+    constructor(){
+        
+    }
+}
+
+
+// Models
 class Customer {
 
     constructor(firstName, lastName){
@@ -32,11 +53,42 @@ class Customer {
 
 class Account {
 
-    constructor(customer, id){
+    constructor(customer, balance, id){
         this.customer = customer;
         this.transactions = [];
-
+        this.id = id;
     }
 
 }
 
+class Transaction{
+
+    constructor(senderAcc, recipientAcc, transAmount){
+        this.senderAcc = senderAcc;
+        this.recipientAcc = recipientAcc;
+        this.transAmount = transAmount;
+    }
+}
+
+
+// Services
+class CustomerService{
+
+    constructor(customer){
+        this.customer = customer;
+    }
+}
+
+class AccountService{
+    
+    constructor(account){
+        this.account = account;
+    }
+}
+
+class TransactionService{
+    
+    constructor(transaction){
+        this.transaction = transaction;
+    }
+}
